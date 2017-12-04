d422269546ce81bf771218a2174d442534d3c462
KasteelMasta <symphonie89@gmail.com>
2017-12-04 22:14:17 +0100

    Initialisation

---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+    public Vector2 speed = new Vector2(50, 50);
+
+    // 2 - Store the movement and the component
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+
+    void Update()
+    {
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Movement per direction
+        movement = new Vector2(
+          speed.x * inputX,
+          speed.y * inputY);
+
+    }
+
+    void FixedUpdate()
+    {
+        // 5 - Get the component and store the reference
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        // 6 - Move the game object
+        rigidbodyComponent.velocity = movement;
+    }
+}
+
+
